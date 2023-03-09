f9cc141373cbf99f270111090304ed6a5402e33d
jonathan briceno <brimanz2014@gmail.com>
2023-03-09 19:43:46 -0400

    adding some styles to character component

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -4,9 +4,13 @@ import './Character.scss'
 const Character = ({character}) =>{
 	return(
 		<div className="app__character text-center p-5">
-	        <h2 className="app__character-title">
-	            {character.name}
-	        </h2>
+			<div className="app__character-info">
+		        <h2 className="app__character-title">
+		            {character.name}
+		        </h2>
+		        <p>{character.status}</p>
+		        <p>{character.species}</p>
+			</div>
 	        <img
 	        	className="app__character-img img-fluid" 
 				src={character.image}
